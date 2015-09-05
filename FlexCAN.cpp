7398ce8a2c5a3320a17fa2e2baf13eda9d4e9aad
H4nky84 <daniel.hancock@gmail.com>
2015-09-05 15:52:06 +1000

    Update FlexCAN.cpp

--- FlexCAN.cpp
+++ FlexCAN.cpp
@@ -142,7 +142,7 @@ int FlexCAN::read(CAN_message_t &msg)
   // get identifier and dlc
   msg.len = FLEXCAN_get_length(FLEXCAN0_MBn_CS(rxb));
   msg.ext = (FLEXCAN0_MBn_CS(rxb) & FLEXCAN_MB_CS_IDE)? 1:0;
-  msg.rtr  = (FLEXCAN0_MBn_CS(rxb) & FLEXCAN_MB_CS_RTR)? 1:0;
+  msg.rtr = (FLEXCAN0_MBn_CS(rxb) & FLEXCAN_MB_CS_RTR)? 1:0;
   msg.id  = (FLEXCAN0_MBn_ID(rxb) & FLEXCAN_MB_ID_EXT_MASK);
   if(!msg.ext) {
     msg.id >>= FLEXCAN_MB_ID_STD_BIT_NO;
